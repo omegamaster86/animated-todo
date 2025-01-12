77127c2da1ae61c3e4fc5f2c56e1b7ac2730c080
omegamaster86 <masya868686@gmail.com>
2025-01-12 17:29:35 +0900

    ダークモードの実装

--- app/screens/main.tsx
+++ app/screens/main.tsx
@@ -1,10 +1,21 @@
-import * as React from 'react';
-import { Text, Box, Center } from 'native-base';
+import * as React from 'react'
+import { Text, Box, Center, VStack } from 'native-base'
+import ThemeToggle from '../../components/theme-toggle'
 
 export default function MainScreeen() {
   return (
-    <Center _dark={{bg: "blueGray.900"}} _light={{bg: "blueGray.50"}} flex={1} px={4}>
-      <Box><Text>hello</Text></Box>
+    <Center
+      _dark={{ bg: 'blueGray.900' }}
+      _light={{ bg: 'blueGray.50' }}
+      flex={1}
+      px={4}
+    >
+      <VStack space={5} alignItems="center">
+        <Box>
+          <Text>hello</Text>
+        </Box>
+        <ThemeToggle />
+      </VStack>
     </Center>
-  );
+  )
 }
